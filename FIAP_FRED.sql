82351df9ed5b0c4ad24084bfe21525622a8febf6
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-18 22:42:17 -0300

    .

--- FIAP_FRED.sql
+++ FIAP_FRED.sql
@@ -1,1 +1,20 @@
-CREATE TABLE ALUNO (ID INT);
+
+/*CRIANDO TABELAS*/
+CREATE TABLE ENDERECO (
+    cep int PRIMARY KEY,
+    rua varchar(100),
+    uf char(2)
+);
+CREATE TABLE ALUNO(
+    ra int,
+    nome varchar (60) CONSTRAINT nn_nome NOT NULL,
+    dt_nascimento date NOT NULL,
+    cpf varchar(14),
+    cep int REFERENCES ENDERECO(cep),
+    CONSTRAINT uk_cpf UNIQUE(cpf),
+    CONSTRAINT pk_ra PRIMARY KEY (ra)
+);
+
+-- APAGANDO TABELAS
+--DROP TABLE ALUNO;
+--DROP TABLE ENDERECO;
